ea773d96b3826efba909253f88e7296ab3f115e1
godot <xugodot@gmail.com>
2020-06-09 14:12:57 +0800

    Fix ssl renew script.

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -4,6 +4,7 @@ WD_DIR = ''
 COMPOSE="/usr/local/bin/docker-compose --no-ansi"
 DOCKER="/usr/bin/docker"
 
-cd WD_DIR
+date
+cd $WD_DIR
 $COMPOSE run certbot renew && $COMPOSE kill -s SIGHUP webserver
 $DOCKER system prune -af
